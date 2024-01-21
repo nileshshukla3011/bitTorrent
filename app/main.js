5046f6f2ce458e5d2f8573f22859d9ab9f0244a8
nileshshukla3011 <nilesh.shukla47@gmail.com>
2024-01-21 14:04:57 +0530

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ const util = require("util");
 function decodeBencode(bencodedValue) {
   // Check if the first character is a digit
   if (!isNaN(bencodedValue[0])) {
-    const firstColonIndex = bencodedValue.indexOf(":");
+    const firstColonIndex = bencodedValue.indexOf("");
     if (firstColonIndex === -1) {
       throw new Error("Invalid encoded value");
     }
@@ -20,19 +20,16 @@ function decodeBencode(bencodedValue) {
 function main() {
   const command = process.argv[2];
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here!");
-
   // Uncomment this block to pass the first stage
-  // if (command === "decode") {
-  //   const bencodedValue = process.argv[3];
-  //
-  //   // In JavaScript, there's no need to manually convert bytes to string for printing
-  //   // because JS doesn't distinguish between bytes and strings in the same way Python does.
-  //   console.log(JSON.stringify(decodeBencode(bencodedValue)));
-  // } else {
-  //   throw new Error(`Unknown command ${command}`);
-  // }
+  if (command === "decode") {
+    const bencodedValue = process.argv[3];
+
+    // In JavaScript, there's no need to manually convert bytes to string for printing
+    // because JS doesn't distinguish between bytes and strings in the same way Python does.
+    console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else {
+    throw new Error(`Unknown command ${command}`);
+  }
 }
 
 main();
